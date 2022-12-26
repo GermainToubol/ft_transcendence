5e7486077aa1c2a06de44111a5704217ef920a35
Germain Toubol <germain.toubol.pro@gmail.com>
2022-12-26 22:17:03 +0100

    [FILES] setup environment for login
    add JWT_SECRET to env
    pass env variables to docker
    install the appropriate modules

--- srcs/backend/src/app.service.ts
+++ srcs/backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!<form action="http://transcendence.42.fr:3000/auth/login" method="POST"><button>coucou</button></form>';
+    }
 }
