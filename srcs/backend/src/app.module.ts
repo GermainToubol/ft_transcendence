dbfd6d83c4522a92fe7d51bc24c69cdbaf5ceb11
Germain TOUBOL <germain.toubol.pro@gmail.com>
2023-01-02 14:27:24 +0100

    [FEAT] connect to the db to get basic informations about the user

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-    imports: [AuthModule],
+    imports: [
+		AuthModule,
+		UsersModule,
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'database.backend-net',
+			port: 5432,
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			entities: [User],
+			synchronize: true,
+		}),
+	],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+	constructor(private dataSource: DataSource) {}
+}
