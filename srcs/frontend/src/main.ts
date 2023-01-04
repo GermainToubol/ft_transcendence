b49ade88f9c90eaf27c3e7a70f763cf5c786890c
Germain TOUBOL <germain.toubol.pro@gmail.com>
2023-01-04 13:34:45 +0100

    [FILES] init new bare front project

--- srcs/frontend/src/main.ts
+++ srcs/frontend/src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
+import './assets/main.css'
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
