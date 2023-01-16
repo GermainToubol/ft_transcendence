670067f3e6e9be392086e8714e84727565a80580
Germain TOUBOL <germain.toubol.pro@gmail.com>
2023-01-16 16:33:10 +0100

    [FEAT] pass the front domain as env parameter and update env.template accordingly

--- srcs/backend/src/auth/auth.controller.ts
+++ srcs/backend/src/auth/auth.controller.ts
@@ -3,36 +3,37 @@ import {
     Get,
     Query,
     Req,
-	UseGuards
+    UseGuards
 } from "@nestjs/common";
 import axios from "axios";
 import { AuthService } from "./auth.service";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 import { ReqUser } from "../users/req-user.decorator";
 import { User } from "../users/user.entity";
+import { jwtConstants } from './constants';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    /* Route: OAuth42 
+    /* Route: OAuth42
         http://${host}:${port}/auth/
     */
     @Get('/login')
     async login(@Query('code') code: string, @Req() req: any) {
-        const token: {access_token: string} = await axios.post('https://api.intra.42.fr/oauth/token', {
+        const token: { access_token: string } = await axios.post('https://api.intra.42.fr/oauth/token', {
             grant_type: "authorization_code", client_id: process.env.API_UID,
             client_secret: process.env.API_SECRET,
             code: code,
-            redirect_uri: 'http://localhost:3500/callback',
-            }).then((t) => t.data);
-        const data: {data: any} = await axios.get('https://api.intra.42.fr/v2/me', {
-                headers: { Authorization: `Bearer ${token.access_token}` },
-            }).then((t) => t.data);
+            redirect_uri: `${jwtConstants.front_domain}/callback`,
+        }).then((t) => t.data);
+        const data: { data: any } = await axios.get('https://api.intra.42.fr/v2/me', {
+            headers: { Authorization: `Bearer ${token.access_token}` },
+        }).then((t) => t.data);
         return this.authService.login(data).then();
     }
 
-	@Get('/validate')
+    @Get('/validate')
     @UseGuards(JwtAuthGuard)
     async validate(@ReqUser() user: string): Promise<any> {
         return user;
