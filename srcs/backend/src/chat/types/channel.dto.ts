7303b4ffdfa8eb195d98b322917bab524fd32256
Germain TOUBOL <germain.toubol.pro@gmail.com>
2023-02-09 13:11:09 +0100

    [REFACTOR] update chat validators

--- srcs/backend/src/chat/types/channel.dto.ts
+++ srcs/backend/src/chat/types/channel.dto.ts
@@ -2,14 +2,14 @@ import { Length, IsEnum, IsString } from "class-validator";
 import { ChannelStatus } from "../channel/channel.entity";
 
 export class ChatChannelDto {
-    @Length(1, 255)
-    @IsString()
-    channelName: string;
+  @Length(1, 255)
+  @IsString()
+  channelName: string;
 
-    @IsEnum(ChannelStatus)
-    channelLevel: ChannelStatus;
+  @IsEnum(ChannelStatus)
+  channelLevel: ChannelStatus;
 
-    @Length(0, 255)
-    @IsString()
-    password: string;
+  @Length(0, 255)
+  @IsString()
+  password: string;
 }
--- srcs/backend/src/chat/types/invitation.dto.ts
+++ srcs/backend/src/chat/types/invitation.dto.ts
@@ -1,11 +1,11 @@
 import { IsNumber, IsString, Length, Min } from "class-validator";
 
 export class InvitationDto {
-    @IsNumber()
-    @Min(0)
-    channelId: number;
+  @IsNumber()
+  @Min(0)
+  channelId: number;
 
-    @IsString()
-    @Length(0)
-    userLogin: string;
+  @IsString()
+  @Length(1, 15)
+  userLogin: string;
 }
